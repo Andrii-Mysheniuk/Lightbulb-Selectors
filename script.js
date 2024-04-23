dcdbbc83e1a0fa95de20d3a197677ea0fddf6227
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-04-23 16:52:17 +0100

    add script

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Write your code here
+const lightbulbs = document.querySelectorAll('.lightbulb');
+const counter = document.querySelector('.subtitle');
+let count = 0;
+
+lightbulbs.forEach(lightbulb => {
+  lightbulb.addEventListener('click', function() {
+    count++;
+    lightbulb.classList.toggle("active");
+    counter.innerHTML = `You've clicked the lights ${count} times`;
+  });
+});
+
+/*lightbulbs.forEach(lightbulb => {
+  lightbulb.addEventListener('mouseover', function() {
+    lightbulb.classList.toggle("active");
+  });
+});*/
+
+/*lightbulbs.forEach(lightbulb => {
+  lightbulb.addEventListener('mouseout', function() {
+    lightbulb.classList.toggle("active");
+  });
+});*/
